aabe031942fdfdad9d1c7b4d6b5c80c054d4c720
Sahadev Sahu <sahadev@techphant.com>
2022-08-10 14:11:14 +0530

    user login request url type change

--- src/routes/users.router.ts
+++ src/routes/users.router.ts
@@ -7,7 +7,7 @@ const usersRouter = Router();
 usersRouter.post('/users/register', validSchema(validateSchema.registerUser) , UsersController.createUser);
 usersRouter.post('/users/forget-password', validSchema(validateSchema.forgetPassword) , UsersController.forgetPassword);
 usersRouter.get('/users/verify-user', validSchema(validateSchema.verifyUserToken), UsersController.verifyUser);
-usersRouter.get('/users/login', validSchema(validateSchema.userLogin), UsersController.userLogin);
+usersRouter.post('/users/login', validSchema(validateSchema.userLogin), UsersController.userLogin);
 usersRouter.put('/users/edit', [validSchema(validateSchema.userEdit), jwtMiddleware] , UsersController.userEdit);
 usersRouter.put('/users/change-password', [ validSchema(validateSchema.changePassword), jwtMiddleware] , UsersController.changePassword);
 usersRouter.get('/users/get-detail', [ jwtMiddleware ] , UsersController.getUserDetail);
